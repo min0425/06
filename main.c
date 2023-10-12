18b1ff4f84fff714859a608032cd2923fb931996
min0425 <2310570@sookmyung.ac.kr>
2023-10-12 17:03:06 +0900

    5

--- main.c
+++ main.c
@@ -2,18 +2,35 @@
 #include <stdlib.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void square(int a)
-{
-	a=a*a;
+
+int combination(int n,int r){
+	int a,b; //a는 분모,b는 분자
+	a=(factorial(n-r))*(factorial(r));
+	b=factorial(n);
+	
+	return b/a; 
 }
 
+int factorial(int a){
+	int res=1;
+	int i;
+	for(i=1;i<=a;i++)
+	res*=i;
+	
+	return res;
+}
 int main(int argc, char *argv[]) {
-	int a=2;
-	square(a);
-	printf("a=%i\n",a);
+	int n,r,result;
+	printf("input number:");
+	scanf("%i %i",&n,&r);
+	
+	result=combination(n,r);
+	printf("C(%i,%i)=%i",n,r,result);
 
 	
 	
 	return 0;
 	
 }
+
+
